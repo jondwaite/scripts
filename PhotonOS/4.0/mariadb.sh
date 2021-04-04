957e8d0d8d93574709c95e06a8248a6d5224f5a8
Jon Waite <jondwaite@users.noreply.github.com>
2021-04-05 01:21:56 +1200

    Add f1db

--- PhotonOS/4.0/mariadb.sh
+++ PhotonOS/4.0/mariadb.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-# Grab and install MariaDB
 
+# https://raw.githubusercontent.com/jondwaite/scripts/main/PhotonOS/4.0/mariadb.sh
+
+tdnf install -y mariadb
 tdnf install -y mariadb-server
+
+# Start and enable the database
+systemctl enable mariadb
+systemctl start mariadb
+
+# Grab the f1 database and add to mariadb
+wget http://ergast.com/downloads/f1db.sql.gz
+gunzip f1db.sql.gz
+mysql -u root -e "CREATE DATABASE f1db"
+mysql -u root f1db < f1db.sql
+
