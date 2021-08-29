df88095a623f610e5e760f4f238cbf986abe87e0
Jon Waite <jondwaite@users.noreply.github.com>
2021-08-29 19:42:16 +1200

    add express

--- PhotonOS/4.0/f1app/db/f1orm/index.ts
+++ PhotonOS/4.0/f1app/db/f1orm/index.ts
@@ -1,16 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+import express from 'express'
 
 const prisma = new PrismaClient()
+const app = express()
 
-async function main() {
-  const allDrivers = await prisma.drivers.findMany()
-  console.log(allDrivers)
-}
+app.get('/drivers', async (req, res) => {
+  const drivers = await prisma.drivers.findMany()
+  res.json(drivers)
+})
 
-main()
-  .catch((e) => {
-    throw e
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+const server = app.listen(3000, () =>
+  console.log(`
+    Server ready at http://localhost:3000`,),
+)
